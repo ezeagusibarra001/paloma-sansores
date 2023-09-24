a339ccf9cee5b19d8b307f6edd25b88a30b1e96c
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2023-09-24 03:41:32 -0300

    fixes

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCjvxG6p_QSpOEBNGsFo71iKaBOw-FD40",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: "paloma-c8fa9.firebaseapp.com",
   projectId: "paloma-c8fa9",
   storageBucket: "paloma-c8fa9.appspot.com",
   messagingSenderId: "1073293763150",
-  appId: "1:1073293763150:web:694d9b56f2bb1d72f8d91f"
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
