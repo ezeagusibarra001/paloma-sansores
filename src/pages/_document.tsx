99533f797f834901f5c1b2859f6947fc5184bff9
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2023-10-17 23:44:32 -0300

    fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Paloma Sansores</title>
+        <meta name="description" content="Soy Paloma Sansores, tengo más de 8 años de emprendimiento en negocios digitales y disfrutando de la libertad financiera. Inicié esta carrera del emprendimiento porque siempre supe como quería vivir mi vida, sabia que quería vivir mi vida bajo mis propios términos." />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
